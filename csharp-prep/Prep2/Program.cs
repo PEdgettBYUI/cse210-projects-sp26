a9fb32d6465bca027292a230e71b039b2e4d0cdf
Patrick Edgett <edg24004@byui.edu>
2026-04-23 14:35:18 -0600

    Created the program description for CSE210 Learning Activity Prep 2 - Grade evaluator;

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,18 @@
+// Patrick Thomas Edgett
+// 4/23/2026
+// This program is a simple grade evaluator. It takes a given numerical value
+// and provides an appropriate letter grade. The program will also account for
+// + & - variations of given letter grades, except for A+, F+, and F-, which are
+// not recognized as valid grades.
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Hello Prep2 World!\n");
+
+        
     }
 }
