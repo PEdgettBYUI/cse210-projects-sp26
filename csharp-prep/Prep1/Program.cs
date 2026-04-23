57ea91a79ad26fadcde68c6d64b5b630333252c2
Patrick Edgett <edg24004@byui.edu>
2026-04-23 14:28:26 -0600

    Completed W01 Prove Part 1 - Prep 1;

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,22 @@
+// Patrick Thomas Edgett
+// 4/23/2026
+// This program is a simple demonstration of reading and writing strings to the console
+
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Hello Prep1 World!\n");
+
+        Console.Write("What is your first name? ");
+        string FirstName_PTE = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string LastName_PTE = Console.ReadLine();
+
+        Console.WriteLine($"\nYour name is {LastName_PTE}, {FirstName_PTE} {LastName_PTE}\n\n");
     }
 }
