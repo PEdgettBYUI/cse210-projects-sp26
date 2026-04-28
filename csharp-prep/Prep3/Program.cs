a5ba4b9cd700c00dcd11cd894896bdc335def027
Patrick Edgett <edg24004@byui.edu>
2026-04-28 11:56:37 -0600

    Created intro for Number Guesser; Implemented a default RNG system;

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Program introduction
         Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("This is the Number Guesser Game");
+        Console.WriteLine("In this game, you will give us the \'Magic Number\', and we will choose a number from 1 to that number\nthat you have to guess!\nAim for the lowest number of tries!");
+
+        // Default RNG Max value
+        int maxNum_PTE = 100;
+
+        // Initialize Magic Number
+        Random randGen_PTE = new Random();
+        int magicNum_PTE = randGen_PTE.Next(1,maxNum_PTE);
+        Console.WriteLine($"Game Difficulty: 1 to {maxNum_PTE}\n\n");
+        // Console.WriteLine($"Magic Num: {magicNum_PTE}\n"); // TEST
     }
 }
