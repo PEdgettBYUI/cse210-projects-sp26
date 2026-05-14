d07db8556d703a7c833827cf85c75e0b0d9c6193
Patrick Edgett <edg24004@byui.edu>
2026-05-14 16:43:09 -0600

    Created NameJournal() for default constructor, completed LoadJournal() for Journal.cs (pending testing);

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -5,6 +5,8 @@
 *    2. 
 *    3. 
 */
+using System.IO;
+
 public class Journal
 {
     private string _journalName_PTE;
@@ -13,7 +15,17 @@ public class Journal
     // Constructor
     public Journal()
     {
-        
+        _journalName_PTE = NameJournal();
+    }
+
+    // Name the Journal (NOTE: Extra Info later?)
+    private string NameJournal()
+    {
+        Console.WriteLine("Please name your Journal: ");
+        string name_PTE = Console.ReadLine();
+
+        Console.WriteLine($"[Journal Created: {name_PTE}");
+        return name_PTE;
     }
 
     // Add an Entry object to the list of Entries.
@@ -30,32 +42,54 @@ public class Journal
         Console.WriteLine($"--Total Entries: {_entries_PTE.Count}--\n");
     }
 
-    // LOAD
+    // LOAD journal from a given filename .text-file
     public void LoadJournal(string filename)
     {
         // Open the given file name
-        // Read each line from the file
-        // Split the line into parts at the '#'
-        // Use the split parts to instance an Entry with the "Loading" constructor
-        // Append the new entry to the current journal's _entries list
+        if (System.IO.File.Exists(filename))
+        {
+            string[] lines_PTE = System.IO.File.ReadAllLines(filename);
+
+            // Read each line from the file
+            foreach (string line in lines_PTE)
+            {
+                // Split the line into parts at the '#'
+                string[] parts_PTE = line.Split("#");
+                // Use the split parts to instance an Entry with the "Loading" constructor
+                string f_date_PTE = parts_PTE[0];
+                string f_prompt_PTE = parts_PTE[1];
+                string f_response_PTE = parts_PTE[2];
+
+                Entry f_entry = new Entry(f_date_PTE, f_prompt_PTE, f_response_PTE);
+                // Append the new entry to the current journal's _entries list
+                this.AddEntry(f_entry);
+            }
+        }
+        else
+        {
+            // Error message
+            Console.WriteLine($"ERROR!\nThe File: \"{filename}\" Does Not Exist within the save directory.\nPlease provide an existing filename.\n\n");
+        }
     }
 
-    // SAVE entries in _entries_PTE<> to a given filename text-file
+    // SAVE entries in _entries_PTE<> to a given filename .text-file
     public void SaveJournal(string filename)
     {
         //Check if the list has entires, if not, Output error message
         if (_entries_PTE.Count >0) {
             // Using StreamWriter, go through _entries and output each Entry's ConvertToString()
-            using (StreamWriter outputfile = new StreamWriter(filename))
+            // NOTE: Streamwriter will create a new file of the given filename.txt if it does not exist already
+            using (StreamWriter outputFile = new StreamWriter(filename + ".txt"))
             {
                 foreach (Entry entry in _entries_PTE)
                 {
-                    outputfile.WriteLine(entry.ConvertToString());
+                    outputFile.WriteLine(entry.ConvertToString());
                 }
             }
         }
         else
         {
+            // Error message
             Console.WriteLine("[NO Entries. SAVE Failed.]");
         }
 
