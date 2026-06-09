5e147361e722ce09d7b3c5d8bdf6a9241201f97d
Patrick Edgett <edg24004@byui.edu>
2026-06-09 15:52:35 -0600

    Progress on Activity.cs;

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -1,21 +1,23 @@
 /* Patrick Thomas Edgett | Jacob Middledorf | 6/9/2026 
  * References
- * 1.
+ * 1. https://chatgpt.com/share/6a2885b0-a200-83e8-931d-d90563abc586 - Get seconds from DateTime
  * 2.
  * 3.
  *
  */
 public class Activity {
-    private string _Name;
-    private string _Description;
-    private int _Duration;
+    private string _Name_PTE;
+    private string _Description_PTE;
+    private int _Duration_PTE;
+    private DateTime _StartTime_PTE;
+    private DateTime _EndTime_PTE;
 
 
     public Activity(string Name, string Description, int Duration)
     {
-        _Name = Name;
-        _Description = Description;
-        _Duration = Duration;
+        _Name_PTE = Name;
+        _Description_PTE = Description;
+        _Duration_PTE = Duration;
     }
 
     public string IntroMessage()
@@ -28,14 +30,20 @@ public class Activity {
         return "TEMP Exit";
     }
 
-    // public void RunActivity()
-    // {
-        // Comeback after Polymorphism
-    // }
-
-    public bool CheckDuration(int CountFrom)
+    public void RunActivity()
     {
-        return true;
+        _StartTime_PTE = DateTime.Now;
+        _EndTime_PTE = _StartTime_PTE.AddSeconds(_Duration_PTE);
+    }
+
+    public bool CheckDuration()
+    {
+        DateTime CurrentTime_PTE = DateTime.Now;
+        if(CurrentTime_PTE > _EndTime_PTE)
+        {
+            return true;
+        }
+        return false;
     }
 
     public void LoadingSpinner()
@@ -47,4 +55,18 @@ public class Activity {
     {
         Console.WriteLine("3...2...1...");
     }
+
+
+    /*  // Comeback after Polymorphism
+    public void RunActivity(void* (int callback()) )
+    {
+        
+        IntroMessage();
+        LoadingSpinner();
+        int TimePassed = 0;
+        while(!CheckDuration(TimePassed)) {
+            TimePassed = callback();
+        }
+    }
+*/
 }
