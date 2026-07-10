86e2259a23a21e0b41b2d05894dbd8fd0acd742f
HardwoodStudios <jacobmiddledorf1@gmail.com>
2026-07-10 11:40:55 -0600

    Menu Changes

--- prove/Develop05/Menu.cs
+++ prove/Develop05/Menu.cs
@@ -5,54 +5,61 @@
 *    2. https://github.com/PEdgettBYUI/cse210-projects-sp26
 *    3. 
 */
+using Develop05;
+
 class Menu
 {
     public void DisplayMenu()
     {
-        Console.WriteLine("      Menu\n1.Create New Entry\n2.Load Journal\n3.Display Current Journal\n4.Save Current Journal\n5.Save Entry & Exit\n");
+        Console.WriteLine("      Menu\n1.Create New Goal\n2.Load Journal\n3.Display Current Journal\n4.Save Current Journal\n5.Save Entry & Exit\n");
     }
 
     // NOTE: Pass in the name of a given journal to call the functions to. 
-    public void ParseUserChoice(int choice, Journal aJournal)
+    public void ParseUserChoice(int choice)
     {   
         switch(choice)
         {
             // Create new entry and add it to the journal.
             case 1:
                 Console.Clear();
-                Entry newEntry_PTE = new Entry();
-                aJournal.AddEntry(newEntry_PTE);
+                Console.WriteLine("      Menu\n1.Create Simple Goal\n2.Create Eternal Goal\n3.Create Checklist Goal");
+                int subChoice = int.Parse(Console.ReadLine());
+                switch(subChoice)
+                {
+                    case 1:
+                        Console.Clear();
+                        Goal Simple = new Goal(Name,Discription,Points);
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Goal Eternal = new Goal(Name,Discription,Points);
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Goal Checklist = new Goal(Name,Discription,Points);
+                        break;
+                }
 
                 Console.Clear();
                 break;
             // Load an existing journal.
             case 2:
-                Console.Write("What's the name of the Journal you want to LOAD? ");
-                string fileInput_PTE = Console.ReadLine();
-                aJournal.LoadJournal(fileInput_PTE);
-
+                
                 PressToContinue();
                 break;
             // Display each entry in the current journal.
             case 3:
-                Console.Clear();
-                aJournal.DisplayAll();
-
+            
                 PressToContinue();
                 break;
             // Save the current journal to a text file.
             // If "case 5" is called, this will guarantee no progress is lost before EXIT.
             case 4:
             case 5:
-                if (aJournal.EntiresCount() > 0)
-                {   
-                    aJournal.SaveJournal();
-                    Console.WriteLine($"[JOURNAL Saved!]\n[SAVED as: {aJournal.getName()}.txt]");
                 
-                    PressToContinue();
-                }
-                else { Console.WriteLine("[NO Entries. SAVE Failed.]"); }
-
+                
+                PressToContinue();
+                
                 Console.Clear();
                 break;
             // Invalid input case
